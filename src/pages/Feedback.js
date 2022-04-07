64db1fd563b7256dd53648fc9e22bb046b5ac639
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-04-07 14:14:57 -0300

    Não passa no teste

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     feedback: '',
+  //   };
+  // }
+
+  // componentDidMount() {
+  //   const { scorePlayer } = this.props;
+  //   const rightAnswer = 'Well Done!';
+  //   const wrongAnswer = 'Could be better...';
+  //   const numberMin = 3;
+  //   if (scorePlayer < numberMin) {
+  //     this.setState({
+  //       feedback: wrongAnswer,
+  //     });
+  //   } if (scorePlayer > numberMin) {
+  //     this.setState({
+  //       feedback: rightAnswer,
+  //     });
+  //   }
+  // }
+
   render() {
-    const { acertos } = this.props;
+    const { scorePlayer } = this.props;
+    // const { feedback } = this.state;
     const rightAnswer = 'Well Done!';
     const wrongAnswer = 'Could be better...';
     const numberMin = 3;
-    const mensagem = acertos >= numberMin ? rightAnswer : wrongAnswer;
+    const mensagem = scorePlayer >= numberMin ? rightAnswer : wrongAnswer;
     return (
       <div>
         <Header />
@@ -16,18 +41,19 @@ class Feedback extends React.Component {
           data-testid="feedback-text"
         >
           { mensagem }
+          {/* { feedback } */}
         </h1>
       </div>
     );
   }
 }
 
-const mapStateToProps = () => ({
-  acertos: state.player.acertos,
+const mapStateToProps = (state) => ({
+  scorePlayer: state.player.score,
 });
 
 Feedback.propTypes = {
-  acertos: PropTypes.string.isRequired,
+  scorePlayer: PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
